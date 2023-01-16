a95d695f66cf39bdd4571da9c2a5846a53d2a348
Cordu7 <calendulagunst@hotmail.com>
2023-01-16 15:56:09 +0000

    initial commit

---
+++ src/main/java/Runner.java
@@ -0,0 +1,10 @@
+public class Runner {
+    public static void main(String[] args){
+        Planet mars = new Planet("Mars", 908973);
+        System.out.println(mars.getName());
+        System.out.println(mars.getSize());
+        System.out.println(String.format("%s is %s",mars.getName(), mars.getSize()));
+        System.out.println(mars.explode());
+
+    }
+}
